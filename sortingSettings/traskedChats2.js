deadab115437def324bad14ea094c0bb7b0b90d0
sinzem <111191960+sinzem@users.noreply.github.com>
2024-12-26 13:26:52 +0200

    Update traskedChats2.js

--- sortingSettings/traskedChats2.js
+++ sortingSettings/traskedChats2.js
@@ -1,6 +1,6 @@
 const trackedChats2 = {
     /* (Example of connecting groups for processing. It is better to use id (in the case of private groups, only id), but it also works by nicknames or names of groups, channels or users) */
-    trackedChats: ["ChatName", "chatnickname", "-1001234567890" , "ChannelName", "1234567890", "sinzem"], 
+    trackedChats: ["ChatName", "chatnickname", "-1001234567890" , "ChannelName", "1234567890", "usernickname"], 
 
     /* (Examples of words for selection, letter case does not matter) */
     controlWords: ["hello", "hi bro", "yes, but", "5 dollars"],
